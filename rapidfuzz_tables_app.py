c5154d2a077d9f6709022ca6f545ff13e4319ee5
CarlosDFZ <carlos.diazfaz@gmail.com>
2025-09-02 09:44:06 -0600

    Entregable 8 - csv file

--- rapidfuzz_tables_app.py
+++ rapidfuzz_tables_app.py
@@ -1,14 +1,19 @@
 from fuzz_functions import execute_dynamic_matching
 import pandas as pd
 
-
-def dataFrameOrDict(results,type='df'):
+def dataFrameOrDict(results, type='df'):
     if type == "df":
         df = pd.DataFrame(results)
         print(df)
     elif type == "dic":
         for item in results:
             print(item)
+
+def guardar_csv(results, nombre_archivo="resultados.csv"):
+    df = pd.DataFrame(results)
+    df.to_csv(nombre_archivo, index=False, encoding="utf-8-sig")
+    print(f"Resultados guardados en '{nombre_archivo}'")
+
 
 params_dict = {
     "server": "localhost",
@@ -22,7 +27,7 @@ params_dict = {
     "src_dest_mappings": {
         "first_name": "nombre",
         "last_name": "apellido",
-        "email" : "email"
+        "email": "email"
     }
 }
 
@@ -31,13 +36,9 @@ resultados = execute_dynamic_matching(params_dict, score_cutoff=70)
 
 while True:
     formato = input("¿Quieres un DataFrame o un diccionario? (df/dic): ")
-    if formato == "df":
-        df = pd.DataFrame(resultados)
-        print(df)
-        break
-    elif formato == "dic":
-        for item in resultados:
-            print(item)
+    if formato in ["df", "dic"]:
+        dataFrameOrDict(resultados, formato)
+        guardar_csv(resultados)
         break
     else:
         print("Formato no válido. Por favor, elige 'df' o 'dic'.")
